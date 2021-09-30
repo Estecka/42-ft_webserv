5156076ec5b4d840f96c63fae2c2416c3e8f5c5a
abaur <abaur@student.42.fr>
2021-09-30 15:34:40 +0200

    Code Cleanup, leak fixing.

--- clibft/clibft.hpp
+++ clibft/clibft.hpp
@@ -6,7 +6,7 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 19:06:25 by abaur             #+#    #+#             */
-/*   Updated: 2021/09/28 17:11:57 by abaur            ###   ########.fr       */
+/*   Updated: 2021/09/30 15:07:50 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,17 @@ namespace ft
 	 * @return	true if a word was extracted.
 	 */
 	bool	ExtractWord(const std::string& src, std::string& outWord, std::string& outRemain);
+
+	/**
+	 * Deletes all pointers in a container.
+	 * Null pointers are safely ignored.
+	 */
+	template <class C>
+	void	DeleteContent(C& container){
+		for (typename C::iterator it=container.begin(); it!=container.end(); it++)
+			if (*it != NULL)
+				delete *it;
+	}
 }
 
 #endif
