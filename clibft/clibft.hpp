0f8dcf0ca5c3586a4e7314084c243e6e556d5d8b
abaur <abaur@student.42.fr>
2021-10-12 17:15:13 +0200

    Added ErrnoException

--- clibft/clibft.hpp
+++ clibft/clibft.hpp
@@ -6,13 +6,14 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 19:06:25 by abaur             #+#    #+#             */
-/*   Updated: 2021/10/05 19:01:10 by abaur            ###   ########.fr       */
+/*   Updated: 2021/10/12 17:17:14 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIBFT_HPP
 #define CLIBFT_HPP
 
+#include "./ErrnoException.hpp"
 #include "./fdstream.hpp"
 #include "./misc.hpp"
 #include "./path.hpp"
