00ad0407d3c67b584361c5f400d659deb623f0a3
Arthur Pitoiset <apitoise@github.com>
2021-11-12 15:04:42 +0100

    add more ruby tests, still have compilation error when submit post form, idk why rn

--- www/ruby_tests/post_test.rb
+++ www/ruby_tests/post_test.rb
@@ -5,6 +5,7 @@ cgi.out {
 	cgi.html {
 		cgi.head{"\n" + cgi.title{"POST FORM"} } +
 		cgi.body{"\n" +
+		cgi.img("./www/home.jpg", "return home", 20, 20) + 
 		cgi.a("http://localhost:8080") {"Return to localhost"} +
 		cgi.form{"\n" +
 			cgi.hr +
